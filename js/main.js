9a527ce950d3ad2543a9a74c281dc1f9768ff406
Nijat Namazzade <42466886+nijatnamazzade@users.noreply.github.com>
2020-05-28 21:50:42 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,75 @@
+
+/* Navbar Shrink */
+
+/*
+$(function(){
+  $(window).scroll(function() {
+     if($(window).scrollTop() >= 100) {
+       $('nav').addClass('scrolled');
+        $('nav').addClass('fixed-top');
+        
+     }
+    else {
+      $('nav').removeClass('scrolled');
+      $('nav').removeClass('fixed-top');
+    }
+  });
+});
+*/
+
+/* Smooth SCrolling */
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 2000, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
+
+
+/* TypeJS components */
+
+new TypeIt(".small-heading", {
+  strings: "The Next Web is another leading blog on the internet which provides technology stuff on the daily basis to internet users. ",
+  speed: 65,
+  waitUntilVisible: true
+}).go();
+
+
+new TypeIt(".articles-text-left-bottom", {
+  strings: "Read more",
+  speed: 120,
+  waitUntilVisible: true
+}).go();
+
+
+
+
+/* Loading Animation */
+
+
+ setTimeout(function(){
+
+
+  $('.loader_bg').fadeToggle();
+ }, 1300);
